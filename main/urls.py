c89095cfb211e2dae5a3731ba792a7e6926cfdf6
webQbe <webcubesolutionslk@gmail.com>
2024-08-31 21:09:55 +0530

    styled main page and created signup & login buttons

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import main_page
+from .views import main_page, SignupView, LoginView  # Assuming you have these views
 
 urlpatterns = [
     path('', main_page, name='main_page'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.views import LoginView as AuthLoginView
 
 # Create your views here.
 
 def main_page(request):
     return render(request, 'main/main.html')
+
+class SignupView(View):
+    def get(self, request):
+        # Your signup logic
+        return render(request, 'signup.html')
+    
+class LoginView(AuthLoginView):
+    template_name = 'login.html'
