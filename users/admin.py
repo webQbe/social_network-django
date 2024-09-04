ecab0e6529f5713c5d2a1cb766c6492729c5e245
webQbe <webcubesolutionslk@gmail.com>
2024-09-04 11:29:55 +0530

    post model created

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from main.models import UserProfile
+from .models import Post
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Post)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_content = models.CharField(max_length=255)
+    upload_image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    post_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.post_id} by {self.user.username}"
 
