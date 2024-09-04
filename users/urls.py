e0e557621f6a46232bacb30c26248c0095ebf5e2
webQbe <webcubesolutionslk@gmail.com>
2024-09-04 11:11:49 +0530

    basic profile page created

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from main.views import main_page
 
 urlpatterns = [
     path('home/', views.home_view, name='home'),
     path('profile/<int:user_id>/', views.profile_view, name='profile'),
+    path('find/', views.find_view, name='find'),
+    path('message/', views.message_view, name='message'),
+    #path('myposts/<int:user_id>/', views.myPosts_view, name='my_posts'),
+    #path('editprofile/<int:user_id>/', views.editProfile_view, name='edit_profile'),
+    path('logout/', main_page, name='logout'),
+    path('search/', views.search_view, name='search_results'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.views import View
+from main.models import UserProfile
+#from .models import UserPosts
 
 # Create your views here.
 
@@ -12,5 +14,32 @@ def home_view(request):
     return render(request, 'users/home.html')
 
 @login_required
-def profile_view(request):
-    return render(request, 'users/profile.html')
+def profile_view(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    user_profile = get_object_or_404(UserProfile, user=user)
+    return render(request, 'users/profile.html', {'user': user, 'user_profile': user_profile})
+
+@login_required
+def find_view(request):
+    return render(request, 'users/find_people.html')
+
+@login_required
+def message_view(request):
+    return render(request, 'users/message.html')
+
+@login_required
+def myPosts_view(request, user_id):
+    user_posts = get_object_or_404(UserProfile, user_id=user_id)
+    return render(request, 'users/myposts.html', {'user_posts':user_posts})
+
+@login_required
+def editProfile_view(request, user_id):
+    user_profile = get_object_or_404(UserProfile, user_id=user_id)
+    return render(request, 'users/edit_profile.html', {'user_profile':user_profile})
+
+@login_required
+def search_view(request):
+    return render(request, 'users/search.html')
+
+
+
