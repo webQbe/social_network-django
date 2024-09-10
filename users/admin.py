ce8c20a87aacdfbe85452eafbaacc2d48b902bb2
webQbe <webcubesolutionslk@gmail.com>
2024-09-10 12:27:39 +0530

    comment model created

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from main.models import UserProfile
 from .models import Post
+from .models import Comment
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Post)
+admin.site.register(Comment)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Post(models.Model):
     post_id = models.AutoField(primary_key=True)
@@ -12,3 +13,17 @@ class Post(models.Model):
         # Ensure post_content is not None
         post_content_preview = self.post_content[:20] if self.post_content else ""
         return f'{self.user.username} - {post_content_preview}'
+
+
+class Comment(models.Model):
+    # Primary key com_id will be auto-created as id field by default
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=255)
+    comment_author = models.CharField(max_length=255)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        # Ensure comment is not None
+        comment_preview = self.comment[:20] if self.comment else ""
+        return f'{self.comment_author} - {comment_preview}'
